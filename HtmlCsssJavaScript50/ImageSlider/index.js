8247710fd1cd5995dbe8cc5dd0c66a8a3b875a87
Rituraj Rai <162144998+riturajrai@users.noreply.github.com>
2024-09-16 12:23:06 +0530

    Update index.js

--- HtmlCsssJavaScript50/ImageSlider/index.js
+++ HtmlCsssJavaScript50/ImageSlider/index.js
@@ -1,66 +1,37 @@
-*{
-    padding: 0;
-}
-body{
-    font-family: Arial, Helvetica, sans-serif;
-   
-}
-.container{
- width: 100%;
- height: 100%;
- background-color: rgb(42, 42, 42);
- display: grid;
- place-items: center;
- justify-content: center;
- 
-}
-.container .sliderbox{
- width: 90%;
- display: flex;
- 
-}
-.container .sliderbox .firstSlider_box{
-    border-radius: 15px;
-    width: 600px;
-    display: flex;
-    background-color:#fff;
-  background-image:linear-gradient(135deg, transparent 0%, #fff 25%, #fff 75%, transparent 100%),
-   url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAICAYAAADED76LAAAAIklEQVQoU2N89+7dfwYsQEhIiBEkzDgkFGDzAbIY2Cv4AACvrBgJjYNGfwAAAABJRU5ErkJggg==);
-    position: relative;
-    align-items: center;
-    overflow: hidden;
-    
-}
-.container .sliderbox .firstSlider_box .oneslider{
-    display: flex;
-    transition: transform 0.5s ease-in-out;
-}
-.container .sliderbox .firstSlider_box .oneslider .content{
-    width: 600px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding-top: 20px;
-   
-}
-.content img {
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
-    
-    
-}
-#prev{
-    width: 30px;
-position: absolute;
-left: 0;
-}
- #next{
-    position: absolute;
-    width: 30px;
-    right: 0;
-}
-p{
-    padding: 10px;
+const slider = document.querySelector(".oneslider")
+const contantEl = document.querySelectorAll(".content")
+const right = document.querySelector("#next")
+const left = document.querySelector("#prev")
+
+let time;
+let currentImage = 1;
+imageSlider()
+right.addEventListener("click",()=>{
+    clearTimeout(time)
+    currentImage++
+    imageSlider()
+})
+
+left.addEventListener("click",()=>{
+    clearTimeout(time)
+    currentImage--
+    imageSlider()
+})
+
+
+
+function imageSlider(){
+
+if(currentImage > contantEl.length){
+    currentImage = 1
+} else if (currentImage < contantEl ){
+    currentImage = contantEl.length
 }
 
+slider.style.transform = `translateX(-${(currentImage -1)*450}px)`
+   time =setTimeout(()=>{
+    currentImage++
+    imageSlider()
+},2000)
+
+}
